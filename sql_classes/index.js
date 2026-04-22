d58553dd7bf8eb5938eed3edbfab99bd59cf0732
ShwetaPandey30 <pandeyshweta780@gmail.com>
2026-04-22 15:27:11 +0530

    adding ejs and routing

--- sql_classes/index.js
+++ sql_classes/index.js
@@ -2,8 +2,11 @@
 const { faker, da } = require('@faker-js/faker');
 const mysql = require("mysql2");
 const express = require("express");
-
 const app = express();
+const path = require("path");
+
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -31,8 +34,8 @@ app.get("/" , (req,res) => {
     try {
     connection.query(q, (err, result)=>{
     if(err) throw err;
-    console.log(result[0]["count(*)"]);
-    res.send(result[0]["count(*)"]);
+    let count = result[0]["count(*)"];
+    res.render("home.ejs", { count });
     });
     }catch(err){
     console.log(err);
@@ -47,15 +50,3 @@ app.listen('8080', () =>{
     console.log("Server is listening to port 8080");
 });
 
-// try {
-//     connection.query(q, [data], (err, result)=>{
-//     if(err) throw err;
-//     console.log(result);
-    
-//     });
-// }catch(err){
-//     console.log(err);
-    
-// }
-
-// connection.end();
