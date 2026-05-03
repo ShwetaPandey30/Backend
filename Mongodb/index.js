51e5436158e5f1f667d89d6e99d673d7e069b17c
ShwetaPandey30 <pandeyshweta780@gmail.com>
2026-05-03 16:01:44 +0530

    find and update

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -23,65 +23,88 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-User.updateOne({name: "Arjun Kapoor"}, {name: "Motilal"}).then(res=>{
+User.findByIdAndUpdate("69f70558854ec6f2c6f3e27a", {gender: "female"})
+.then(res=>{
     console.log(res)
 
 }).catch(err=>{
     console.log(err);
     
 })
-User.find({ age:{ $gt: 35 }}).then(res=>{
+User.findOneAndUpdate({age: {$gt:35}}, {email: "motilal@yahoo.in"}).then(res=>{
     console.log(res)
 
 }).catch(err=>{
     console.log(err);
     
 })
-User.findById("69f7047b2cace8b4d6673203").then(res=>{
-    console.log(res)
+// User.updateMany({age: {$gt:35}}, {age: 55}).then(res=>{
+//     console.log(res)
 
-}).catch(err=>{
-    console.log(err);
+// }).catch(err=>{
+//     console.log(err);
     
-})
-User.find({_id: "69f7047b2cace8b4d6673203"}).then(res=>{
-    console.log(res)
+// })
 
-}).catch(err=>{
-    console.log(err);
+// User.updateOne({name: "Arjun Kapoor"}, {name: "Motilal"}).then(res=>{
+//     console.log(res)
+
+// }).catch(err=>{
+//     console.log(err);
     
-})
+// })
+// User.find({ age:{ $gt: 35 }}).then(res=>{
+//     console.log(res)
 
-const user1 = User({
-    name: "Janhavi Kapoor",
-    age: 34,
-    email: "jaan@yahoo.in",
-    gender: "female"
-})
-
-user1.save();
-
-const user2 = User({
-    name: "Arjun Kapoor",
-    age: 45,
-    email: "arjun@yahoo.in",
-    gender: "male"
-})
-
-user2.save().then((res)=>{
-    console.log(res);
+// }).catch(err=>{
+//     console.log(err);
     
-}).catch((err)=>{
-    console.log(err)
-})
+// })
+// User.findById("69f7047b2cace8b4d6673203").then(res=>{
+//     console.log(res)
 
-User.insertMany([
-    {name:"Khusi", age: 35, email:"khushi.1@gmail.com", gender: "female"},
-    {name:"Varun Dhawan", age: 35, email:"varun.1@gmail.com", gender: "male"}
-
-]).then(res=>{
-    console.log(res);
-}).catch(err=>{
-    console.log(err);
+// }).catch(err=>{
+//     console.log(err);
     
-})
+// })
+// User.find({_id: "69f7047b2cace8b4d6673203"}).then(res=>{
+//     console.log(res)
+
+// }).catch(err=>{
+//     console.log(err);
+    
+// })
+
+// const user1 = User({
+//     name: "Janhavi Kapoor",
+//     age: 34,
+//     email: "jaan@yahoo.in",
+//     gender: "female"
+// })
+
+// user1.save();
+
+// const user2 = User({
+//     name: "Arjun Kapoor",
+//     age: 45,
+//     email: "arjun@yahoo.in",
+//     gender: "male"
+// })
+
+// user2.save().then((res)=>{
+//     console.log(res);
+    
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// User.insertMany([
+//     {name:"Khusi", age: 35, email:"khushi.1@gmail.com", gender: "female"},
+//     {name:"Varun Dhawan", age: 35, email:"varun.1@gmail.com", gender: "male"}
+
+// ]).then(res=>{
+//     console.log(res);
+// }).catch(err=>{
+//     console.log(err);
+    
+// })
