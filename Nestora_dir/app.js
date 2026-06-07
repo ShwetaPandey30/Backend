bb560b6a9eda52429f71fb98de21e5cc3067d8dd
ShwetaPandey30 <pandeyshweta780@gmail.com>
2026-06-07 14:50:45 +0530

    New and Create Route

--- Nestora_dir/app.js
+++ Nestora_dir/app.js
@@ -46,13 +46,23 @@ app.get("/listings",async(req,res)=>{
     
     
 })
+// ----New route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
 //  Show Route---------------------------------
 app.get("/listings/:id", async(req,res)=>{
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs" , { listing });
 })
-
+// Create Rout------------------------------
+app.post("/listings", async(req, res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
+    
+});
 app.listen(8080,() =>{
     console.log("Server is Listening to port 8080")
 })
