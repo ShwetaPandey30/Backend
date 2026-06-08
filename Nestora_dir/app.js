7af0526a3b46f73c69c8ba6c286c2cb332a03b7f
ShwetaPandey30 <pandeyshweta780@gmail.com>
2026-06-08 12:39:47 +0530

    Edit and Update Route

--- Nestora_dir/app.js
+++ Nestora_dir/app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-
+const methodOverride= require("method-override");
 
 // -----------------Connection to database----------------------
 const MONGO_URL = 'mongodb://127.0.0.1:27017/nestora';
@@ -34,7 +34,7 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}))
-
+app.use(methodOverride("_method"));
 app.get("/", (req,res)=>{
    res.send("Hi I am root!");
 })
@@ -63,6 +63,19 @@ app.post("/listings", async(req, res)=>{
     res.redirect("/listings")
     
 });
+// Edit Route
+app.get("/listings/:id/edit", async(req,res)=>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs" ,{ listing });
+});
+
+//Update Route--------------------------------
+app.put("/listings/:id", async(req,res)=>{
+    let{ id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+})
 app.listen(8080,() =>{
     console.log("Server is Listening to port 8080")
 })
