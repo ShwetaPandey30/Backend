6953da9b26560ef7daaf3780e2f85f8e391e0bd1
ShwetaPandey30 <pandeyshweta780@gmail.com>
2026-06-08 13:19:08 +0530

    ejs-mate

--- Nestora_dir/app.js
+++ Nestora_dir/app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride= require("method-override");
-
+const ejsMate = require("ejs-mate");
 // -----------------Connection to database----------------------
 const MONGO_URL = 'mongodb://127.0.0.1:27017/nestora';
 
@@ -34,7 +34,9 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}))
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method"))
+app.engine("ejs", ejsMate);
+
 app.get("/", (req,res)=>{
    res.send("Hi I am root!");
 })
